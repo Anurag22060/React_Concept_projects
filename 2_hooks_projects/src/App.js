f3aeada464755664d85a7bade2fc5b05f3ca4f00
Anurag22060 <anuragsawal03@gmail.com>
2023-12-20 12:39:38 +0530

    Hooks Used Projects

---
+++ 2_hooks_projects/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import UseState from './Hooks/useState';
+import UseEffect from './Hooks/UseEffect';
+import UseReducer from './Hooks/UseReducer';
+
+
+
+const App = () => {
+  return (
+    <div>
+   
+    {/* <UseReducer/> */}
+    {/* <UseEffect/> */}
+    <UseState/>
+
+    </div>
+  )
+}
+
+export default App;
